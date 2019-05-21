623d51fd69b93691bcc7507b21690ceeb16b06d1
Andriy V. Koval <koval.andrey@gmail.com>
2019-05-21 11:16:11 -0700

    support for pivot

--- analysis/eda-2/eda-2.R
+++ analysis/eda-2/eda-2.R
@@ -29,7 +29,7 @@ dto %>% pryr::object_size(); dto %>% class(); dto %>% names()
 
 # assign aliases for this report
 ds          <- dto$person_date_offense
-ds %>% dplyr::glimpse(50)
+ds %>% dplyr::glimpse(100)
 # ----- custom-functions --------------------------------------
 get_a_sample <- function(
   d,
@@ -54,19 +54,7 @@ get_a_sample <- function(
 #   dplyr::filter(n_offenses > 1L) %>% 
 #   get_a_sample("person_id", 500)
 
-# ---- tweak-data ---------------------------------------------------------------
-
-ds <- ds %>% 
-  dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% # for testing
-  dplyr::group_by(person_id) %>% 
-  dplyr::mutate(
-    drug_ever         = ifelse( sum(n_drug_related) > 0L, TRUE, FALSE )
-    ,drug_after_1996  = ifelse( sum(n_drug_related_after_1996) > 0L, TRUE, FALSE )
-    ,drug_before_1996 = ifelse( (drug_ever==TRUE & drug_after_1996==FALSE), TRUE, FALSE )
-  )
-
-
-# ---- basic-graph -------------------------------------------------------
+# this function will be applied at every aggregation level
 aggregate_by_groups <- function(d){
   d_aggregate <- d %>% 
     dplyr::summarize(
@@ -79,12 +67,33 @@ aggregate_by_groups <- function(d){
     dplyr::ungroup()
   return(d_aggregate)
 }
+# How to use
+# ds %>% 
+#   dplyr::group_by(person_id, offense_arrest_cd) %>% 
+#   aggregate_by_groups()
 
-t1 <- dto$person_date_offense_cd %>% 
-  dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% # for testing
+# ---- tweak-1 ---------------------------------------------------------------
+ds <- ds %>% 
+  # dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% # for testing
+  dplyr::group_by(person_id) %>% 
+  dplyr::mutate(
+    drug_ever         = ifelse( sum(n_drug_related) > 0L, TRUE, FALSE )
+    ,drug_after_1996  = ifelse( sum(n_drug_related_after_1996) > 0L, TRUE, FALSE )
+    ,drug_before_1996 = ifelse( (drug_ever==TRUE & drug_after_1996==FALSE), TRUE, FALSE )
+  )
+
+ds %>% 
+  dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% 
+  neat() 
+
+# ---- tweak-2 ---------------------------------------------------------------
+# let us aggregate ove (YEAR) and (OFFENSE GROUP)
+ds_pivot <- dto$person_date_offense_cd %>% 
+  # dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% # for testing
   dplyr::mutate(
     year = lubridate::year(begin_date)
   ) %>% 
+  # agg level 1 : person   - date - offense group
   dplyr::group_by(person_id, year, offense_group) %>% 
   aggregate_by_groups() %>% 
   dplyr::ungroup() %>%
@@ -94,18 +103,96 @@ t1 <- dto$person_date_offense_cd %>%
     ,drug_after_1996  = ifelse( sum(n_drug_related_after_1996) > 0L, TRUE, FALSE )
     ,drug_before_1996 = ifelse( (drug_ever==TRUE & drug_after_1996==FALSE), TRUE, FALSE )
   ) %>% 
-  dplyr::ungroup() 
-  
+  dplyr::ungroup() %>% 
+  dplyr::group_by(year, offense_group) %>% 
+  dplyr::summarize(
+    n_offense_counts           = sum(n_offense_counts)
+    ,n_convictions             = sum(n_convictions)
+    ,n_drug_related            = sum(na.omit(n_drug_related))
+    ,n_after_1996              = sum(na.omit(n_after_1996))
+    ,n_drug_related_after_1996 = sum(na.omit(n_drug_related_after_1996))
+  ) %>% 
+  dplyr::ungroup()
 
-t1 %>% 
-  dplyr::filter(year  %in% 1970:2014) %>% 
-  dplyr::filter(offense_group %in% c("A","B","C","D")) %>% 
-  ggplot2::ggplot(aes(x = year, y = n_people, color = offense_group))+
-  geom_point()+
-  geom_line(aes(group = offense_group))+
-  geom_vline(xintercept = 1996)+
-  theme_minimal() +
-  labs(title = "Number of people with at least 1 conviction")
+ds_pivot %>% 
+  neat() 
+# ---- basic-pivot -------------------------------------------------------
+
+ds_pivot %>% 
+  rpivotTable::rpivotTable(
+    # rows = c("", "county")
+    # , cols = c("year")
+    # , vals = "n_trained"
+    # , aggregatorName = "Integer Sum"
+    # , rendererName = "Heatmap"
+    # , width="100%"
+    # , height="400px"
+  )
+
+# ---- publish ---------------------------------
+
+path_report_1 <- "./analysis/eda-2/eda-2.Rmd"
+path_report_2 <- "./analysis/eda-2/eda-2-pivot.Rmd"
+
+
+# allReports <- c(path_report_1,path_report_2)
+allReports <- c(path_report_1)
+# allReports <- c(path_report_2)
+
+pathFilesToBuild <- c(allReports)
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+# Build the reports
+for( pathFile in pathFilesToBuild ) {
+  
+  rmarkdown::render(input = pathFile,
+                    output_format=c(
+                      "html_document" # set print_format <- "html" in seed-study.R
+                      # "pdf_document"
+                      # ,"md_document"
+                      # "word_document" # set print_format <- "pandoc" in seed-study.R
+                    ),
+                    clean=TRUE)
+}
+
+
+# ---- basic-graph -------------------------------------------------------
+
+# ds1 <- dto$person_date_offense_cd %>% 
+#   dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% # for testing
+#   dplyr::mutate(
+#     year = lubridate::year(begin_date)
+#   ) %>% 
+#   # agg level 1 : person   - date - offense group
+#   dplyr::group_by(person_id, year, offense_group) %>% 
+#   aggregate_by_groups() %>% 
+#   dplyr::ungroup() %>%
+#   dplyr::group_by(person_id) %>%
+#   dplyr::mutate(
+#     drug_ever         = ifelse( sum(n_drug_related) > 0L, TRUE, FALSE )
+#     ,drug_after_1996  = ifelse( sum(n_drug_related_after_1996) > 0L, TRUE, FALSE )
+#     ,drug_before_1996 = ifelse( (drug_ever==TRUE & drug_after_1996==FALSE), TRUE, FALSE )
+#   ) %>% 
+#   dplyr::ungroup() 
+#  
+# ds1 %>% 
+#   dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% 
+#   neat(caption = "Grouped by : (PERSON) - (DATE) - (OFFENSE GROUP)")
+# 
+# small_sample <- ds1 %>% get_a_sample("person_id",500)
+# 
+# g1 <- ds1 %>% 
+#   dplyr::filter(year  %in% 1970:2014) %>% 
+#   dplyr::filter(offense_group %in% c("A","B","C","D")) %>% 
+#   ggplot2::ggplot(aes(x = year, y = n_people, color = offense_group))+
+#   geom_point()+
+#   geom_line(aes(group = offense_group))+
+#   geom_vline(xintercept = 1996)+
+#   theme_minimal() +
+#   labs(title = "Number of people with at least 1 conviction")
+# 
+# t1 %>% 
+#   dplyr::filter(person_id %in% c(46222,65392, 50495) ) %>% 
+#   neat() 
 
 # ---- define-utility-functions ---------------
 
